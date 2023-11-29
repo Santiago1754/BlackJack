7e7470f9a1fa1a2b4096ea6be0720936ff727f1a
qphanthaamat <qphanthaamat@gmail.com>
2023-11-29 15:51:03 -0800

    Update

--- src/Hand.java
+++ src/Hand.java
@@ -1,104 +1,68 @@
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hand {
 
- //  Private Variables
- private Vector<Card> cards;
- private int handTotal;
+    // Private Variables
+    private List<Card> cards;
+    private int handTotal;
 
-  //  Constructor
-  public Hand(){
-    handTotal = 0;
-    cards = new Vector<>();
-  }
-
-  public Card returnLatest(){
-    return cards.get(cards.size()-1);
-  }
-  // Setters
-  public void setHandTotal(int handTotal) {
-    this.handTotal = handTotal;
-  }
-
-  public int returnNumCards(){
-    return cards.size();
-  }
-
-  // Getters
-  public int getHandTotal() {
-    
-    return handTotal;
-  }
-
-  //  Other Methods
-  
-  // append a card to the hand given by the dealer
-  public void addCardToHand(Card card) {
-    //  add card to Hand vector
-
-     if (card.getcardValue() == 1 && handTotal<11){
-
-      handTotal += 11;
-
-    }
-    else if (card.getcardValue() > 10){
-      handTotal += 10;
-    }
-    else{
-      handTotal += card.getcardValue();
-    }
-  
-    cards.add(card);
-
-    
-     
-       
-  }
-
-/*  public void reinitCardValue(){
-
-    int numAces = 0;
-    handTotal = 0;
-
-    for (int i=0;i<cards.size();i++){
-      if (cards.get(i).isAce == true){
-        numAces++;
-      }
-      else if (cards.get(i).getcardValue() > 10){
-        handTotal+=10;
-      }
-      else{
-        handTotal+=cards.get(i).getcardValue();
-      }
+    // Constructor
+    public Hand() {
+        handTotal = 0;
+        cards = new ArrayList<>();
     }
 
-    if (numAces==1 && handTotal<=10){
-      handTotal+=11;
-    }
-    else{
-      
-      for (int i =0;i<numAces;i++){
-        handTotal+=1;
-      }
+    // Get the latest card in the hand
+    public Card getLatest() {
+        return cards.get(cards.size() - 1);
     }
 
-  }
-*/
-  public Card accessCardatIndex(int i){
-    return cards.get(i);
-  }
-
-  public void setEqualTo(Hand h){
-    cards.clear();
-
-    for (int i=0;i<h.returnNumCards();i++){
-     
-
-      addCardToHand(h.accessCardatIndex(i));
-      
+    // Set the total value of the hand
+    public void setHandTotal(int handTotal) {
+        this.handTotal = handTotal;
     }
 
-   }
+    // Get the number of cards in the hand
+    public int getNumCards() {
+        return cards.size();
+    }
 
+    // Get the total value of the hand
+    public int getHandTotal() {
+        return handTotal;
+    }
 
+    // Add a card to the hand and update the hand's total value
+    public void addCardToHand(Card card) {
+        if (card.getCardValue() == 1 && handTotal < 11) {
+            // If the card is an Ace and its value as 11 won't bust, use 11
+            handTotal += 11;
+        } else if (card.getCardValue() > 10) {
+            // If the card is a face card, use its value as 10
+            handTotal += 10;
+        } else {
+            // Otherwise, use the numeric value of the card
+            handTotal += card.getCardValue();
+        }
+
+        // Add the card to the hand
+        cards.add(card);
+    }
+
+    // Access a card at a specific index in the hand
+    public Card accessCardAtIndex(int index) {
+        return cards.get(index);
+    }
+
+    // Set the hand equal to another hand by copying its cards
+    public void setEqualTo(Hand otherHand) {
+        // Clear the current cards in the hand
+        cards.clear();
+
+        // Copy cards from the other hand to this hand
+        for (int i = 0; i < otherHand.getNumCards(); i++) {
+            addCardToHand(otherHand.accessCardAtIndex(i));
+        }
+    }
 }
