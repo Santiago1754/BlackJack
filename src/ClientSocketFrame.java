d012e9bdc00d70a067c1bbf49fafda37778ba7dc
eper2016 <89178861+eper2016@users.noreply.github.com>
2023-12-03 00:01:06 -0800

    Update ClientSocketFrame.java

--- src/ClientSocketFrame.java
+++ src/ClientSocketFrame.java
@@ -16,23 +16,28 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ClientSocketFrame extends JFrame implements ActionListener  {
-    JLabel userLabel;
+    JLabel userLabel, socketLabel;
     JLabel passwordLabel, role, roleChose;
     JLabel loginMessage, errorMessage;
     JTextField userTextField;
     JPasswordField passTextField;
     JTextField txtMark;
     JButton loginButton, createAccountButton, playerButton, dealerButton;
-
-	Account account = new Account(); 
+    JButton hitButton, standButton; 
+    Account account = new Account(); 
     
    
    
     public ClientSocketFrame() {
+    	
         this.setTitle("BlackJack Login");
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
+        
+        socketLabel = new JLabel(""); 
+        socketLabel.setBounds(10, 20, 400, 25);
+        add(socketLabel); 
 
         userLabel = new JLabel("User ID");
         userLabel.setBounds(10, 20, 80, 25);
@@ -51,12 +56,12 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
         add(passTextField);
 
         loginButton = new JButton("LOGIN");
-        loginButton.setBounds(10, 120, 200, 25);
+        loginButton.setBounds(20, 120, 100, 25);
         loginButton.addActionListener(this);
         add(loginButton);
         
-        createAccountButton = new JButton("Create An Account"); 
-        createAccountButton.setBounds(10, 140, 200, 25);
+        createAccountButton = new JButton("REGISTER"); 
+        createAccountButton.setBounds(160, 120, 100, 25);
         createAccountButton.addActionListener(this);
         add(createAccountButton); 
         
@@ -73,18 +78,29 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 		
 		
 		 role = new JLabel("PICK A ROLE"); 
-	     role.setBounds(10,20,80,25);
+	     role.setBounds(10,50,80,25);
 	     
 	     playerButton = new JButton("PLAYER");
-	     playerButton.setBounds(100, 20, 165, 25);
+	     playerButton.setBounds(100, 50, 165, 25);
 	     playerButton.addActionListener(this); 
 	       
 	     dealerButton = new JButton("DEALER");
-	     dealerButton.setBounds(100, 50, 165, 25);
+	     dealerButton.setBounds(100, 80, 165, 25);
 	     dealerButton.addActionListener(this); 
 	     
 	     roleChose = new JLabel("");
-	     roleChose.setBounds(100, 80, 165, 25);
+	     //roleChose.setBounds(10, 80, 165, 25);
+	     
+	     
+	     /*------------------------------------------------------------------------*/
+	     
+	     hitButton = new JButton("HIT"); 
+	     hitButton.setBounds(150, 20, 80, 25);
+	     hitButton.addActionListener(this);
+	     
+	     standButton = new JButton("STAND"); 
+	     standButton.setBounds(250, 20, 80, 25);
+	     standButton.addActionListener(this);
 	     
 	        
 			
@@ -100,6 +116,7 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
     	Socket s;
 		try {
 			s = new Socket("localhost", 5000);
+			socketLabel.setText(s.toString());
 			if (e.getSource().equals(loginButton)) {
 	            try {
 	                int l = processInformation(s); 
@@ -151,7 +168,7 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 	        {
 	        	playerButton.hide();
 	        	dealerButton.hide();
-	        	roleChose.setBounds(100, 20, 165, 25);
+	        	roleChose.setBounds(10, 20, 165, 25);
 	        	// roleChose.setText("I AM A DEALER");
 				dealerPlays(s); 
 	        }
@@ -159,8 +176,8 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 	        {
 	        	playerButton.hide();
 	        	dealerButton.hide();
-	        	roleChose.setBounds(100, 20, 165, 25);
-	        	roleChose.setText("I AM A PLAYER");
+	        	roleChose.setBounds(10, 20, 165, 25);
+	        	// roleChose.setText("I AM A PLAYER");
 				playerPlays(s); 
 	        }
 		} catch (IOException | ClassNotFoundException e1) 
@@ -185,6 +202,7 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 			String userName = userTextField.getText();
 			String passWord = passTextField.getText(); 
 			String combo = userName + " " + passWord; 
+			account.setName(userName.toUpperCase());
 			
 			Message m1, m2; 
 			m1 = new Message("LOGIN", "", combo); 
@@ -242,7 +260,7 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 			String combo = userName + " " + passWord; 
 			
 			Message m1, m2; 
-			m1 = new Message("CREATE", "", combo); 
+			m1 = new Message("REGISTER", "", combo); 
 			
 			
 			oos.writeObject(m1); 
@@ -278,8 +296,9 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
     
     public void MainFrame() 
     {
+    	//Message messageToServer = new Message("");
     	this.setTitle("BlackJack Main");
-        this.setSize(1000, 1000);
+        this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
@@ -296,12 +315,7 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 		add(playerButton);
 		add(dealerButton);
 		add(roleChose); 
-		
-		
-		
-		
-		
-		
+
         this.setVisible(true);
     	
     }
@@ -320,14 +334,24 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 		
 		messageFromServer = (Message)ois.readObject(); 
 		
-		role.hide();
-		roleChose.setText(messageFromServer.getData() + " IS A " + messageFromServer.getStatus());
+		if (messageFromServer.getType().contains("BAD"))
+		{
+			role.setText("Sorry! The game currently has a dealer. You are now a " + messageFromServer.getStatus());
+			account.setRole(messageFromServer.getStatus());
+			roleChose.setText(account.getName() + "IS A " + account.getRole()); 
+		}
 		
+		else 
+		{
+			role.hide();
+			roleChose.setText(account.getName() + " IS A " + messageFromServer.getStatus());
+		}	
 	}
     
-    public void playerPlays(Socket s) throws IOException, ClassNotFoundException{
-		
-		ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+    public void playerPlays(Socket s) throws IOException, ClassNotFoundException 
+    {
+    	
+    	ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
 		ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
 
     	Message message = new Message("ROLE", "PLAYER", "");
@@ -340,12 +364,13 @@ public class ClientSocketFrame extends JFrame implements ActionListener  {
 		
 		account.setRole(messageFromServer.getStatus());
 		role.hide();
+		socketLabel.hide();
 		roleChose.setText(account.getName() + " IS A " + account.getRole());
 		
-
-	}
-
-    
-
+		
+		add(hitButton);
+		add(standButton);
 	
+	}
+    
 }
