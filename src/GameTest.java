42d936998e538e5f2a170e8541007f13b21f10b9
Santiago1754 <89170378+Santiago1754@users.noreply.github.com>
2023-12-04 21:18:28 -0800

    Create GameTest.java
    Made a JUnit to test the methods of the Game class.

---
+++ src/GameTest.java
@@ -0,0 +1,62 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	private Game game;
+    private Player player1;
+    private Player player2;
+    private Dealer dealer;
+    private Scoreboard scoreboard;
+    private Deck[] decks;
+	
+	@BeforeEach
+	void setUP() {
+		player1 = new Player();
+        player2 = new Player();
+        dealer = new Dealer();
+        scoreboard = new Scoreboard(new Score[]{});
+        decks = new Deck[]{new Deck(), new Deck()};
+
+        Player[] players = {player1, player2};
+
+        game = new Game("TestGame", players, dealer, scoreboard, decks);
+	}
+	
+	@Test
+	void testMethods() {
+		
+		assertEquals("TestGame", game.getId());
+		assertArrayEquals(new Player[] {player1, player2}, game.getPlayers());
+		assertEquals(dealer, game.getDealer());
+		assertEquals(scoreboard, game.getScoreboard());
+		assertArrayEquals(decks, game.getDecks());
+	}
+	
+	@Test
+	void testHasDealer() {
+		assertTrue(game.hasDealer());
+		game.setDealer(null);
+		assertFalse(game.hasDealer());
+	}
+	
+	@Test
+	void testAddPlayer() {
+		Player newPlayer = new Player();
+		game.addPlayer(newPlayer);
+		
+		Player[] expected = {player1, player2, newPlayer};
+		assertArrayEquals(expected, game.getPlayers());
+	}
+	
+	@Test
+	void testRemovePlayer() {
+		game.removePlayer(player1.getName());
+		
+		Player[] expected = {player1, player2, newPlayer};
+		assertArrayEquals(expected, game.getPlayers());
+	}
+	
+}
